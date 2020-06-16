6bc932f4e1690bf31da7271ca83931fde97f9f3e
Shaun Halliday <shaunhalliday@macbook-pro.home>
2020-06-16 17:17:49 +0100

    Completed tutorials, asteroid vue app and deployed build completed

--- asteroids/src/App.vue
+++ asteroids/src/App.vue
@@ -1,28 +1,47 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <AsteroidGrid
+      @remove="remove"
+      :asteroids="asteroids"
+      header="Near-Earth Objects"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AsteroidGrid from "./components/AsteroidGrid.vue"
+import axios from 'axios'
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    AsteroidGrid,
+  },
+  data() {
+    return {
+      asteroids: [],
+    };
+  },
+  created: function() {
+    this.fetchAsteroids();
+  },
+  methods: {
+    fetchAsteroids: function() {
+      var apiKey = "lkgI9to0hRizfzk4xTAxtNTTFkkA4Mtq7y1yW5me";
+      var url = "https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=" + apiKey;
+      axios.get(url).then((res) => {
+        this.asteroids = res.data.near_earth_objects.slice(0, 10);
+      });
+    },
+    remove: function(index) {
+      this.asteroids.splice(index, 1);
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+[v-cloak] {
+  display: none;
 }
 </style>
